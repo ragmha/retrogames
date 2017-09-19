8a80a10d24e1b97880ebbd0ef878e02cc99c361f
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-19 17:47:09 +0300

    update Contact component

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,3 +1,13 @@
 import React from 'react';
 
+const Contact = () => (
+  <div className="inner cover">
+    <h1 className="cover-heading">Any Questions?</h1>
+    <p className="lead">
+      Send PR to 👉 {' '}
+      <a href="https://github.com/ragmha/retrogames">retrogames.com</a>
+    </p>
+  </div>
+);
+
 export default Contact;
