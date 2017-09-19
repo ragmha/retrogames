cd9a5f7d3c76a40000716065731788cfec7c80fa
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-20 00:57:26 +0300

    make redux avaialable to all components ✨

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
 import { Home, Welcome, About, Contact, Archive } from './components';
 import { AddGame, Games } from './containers';
 
+const store = configureStore();
+
 const routes = (
-  <Router history={hashHistory}>
-    <Route path="/" component={Home}>
-      <IndexRoute component={Welcome} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-    </Route>
-    <Route path="/games" component={Archive}>
-      <IndexRoute component={Games} />
-      <Route path="add" component={AddGame} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={Home}>
+        <IndexRoute component={Welcome} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Route>
+      <Route path="/games" component={Archive}>
+        <IndexRoute component={Games} />
+        <Route path="add" component={AddGame} />
+      </Route>
+    </Router>
+  </Provider>
 );
 
 export default routes;
